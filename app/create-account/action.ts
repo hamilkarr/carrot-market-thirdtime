2c8cf5de4d8fafec2b16d69cc3d7a47e1ff9ff3e
hamilkarr <12uclock@gmail.com>
2025-01-09 14:54:13 +0900

    # 의존성 패키지 업데이트 및 계정 생성 로직 개선 - 사용자명과 이메일 중복 검사 기능 추가 - bcrypt를 이용한 비밀번호 해싱 구현 - iron-session을 활용한 세션 관리 기능 통합

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -8,7 +8,12 @@ import {
   USERNAME_MAX_LENGTH,
   USERNAME_MIN_LENGTH,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { cookies } from 'next/headers';
+import { getIronSession } from 'iron-session';
+import { redirect } from 'next/navigation';
 
 const checkPassword = ({
   password,
@@ -20,7 +25,29 @@ const checkPassword = ({
   return password === confirm_password;
 };
 
-const passwordRegex = PASSWORD_REGEX;
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user) === false;
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user) === false;
+};
 
 const formSchema = z
   .object({
@@ -33,16 +60,22 @@ const formSchema = z
       .max(USERNAME_MAX_LENGTH)
       .toLowerCase()
       .trim()
-      .transform((username) => {
-        return `🔥 ${username} 🔥`;
-      }),
-    email: z.string().email().toLowerCase().trim(),
+      .refine(checkUniqueUsername, 'This username is already taken'),
+    // .transform((username) => {
+    //   return `🔥 ${username} 🔥`;
+    // }),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .trim()
+      .refine(checkUniqueEmail, 'This email is already taken'),
     password: z
       .string({
         required_error: PASSWORD_REQUIRED_ERROR,
       })
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+      .min(PASSWORD_MIN_LENGTH),
+    // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPassword, {
@@ -57,10 +90,35 @@ export const createAccount = async (prevState: any, formData: FormData) => {
     password: formData.get('password'),
     confirm_password: formData.get('confirm_password'),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const hashedPassword = await bcrypt.hash(result.data.password, 4);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    // log the user in
+    // const session = await getIronSession(cookies(), {
+    //   cookieName: 'delicious-karrot',
+    //   password: process.env.COOKIE_PASSWORD!,
+    // });
+
+    const session = await getIronSession(await cookies(), {
+      cookieName: 'delicious-karrot',
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    // @ts-ignore
+    session.id = user.id;
+    await session.save();
+    redirect('/profile');
   }
 };
