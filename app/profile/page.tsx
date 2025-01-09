a5bca27536ed6ad1157efd482d5dc4ab8884e35b
hamilkarr <12uclock@gmail.com>
2025-01-09 17:07:21 +0900

    프로필 페이지: 세션 관리 및 로그아웃 기능 추가

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,36 @@
-const Profile = () => {
-  return <div>Profile</div>;
+import getSession from '@/lib/session';
+import db from '@/lib/db';
+import { notFound, redirect } from 'next/navigation';
+
+const getUser = async () => {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) return user;
+  }
+  notFound();
+};
+
+const Profile = async () => {
+  const user = await getUser();
+  const logOut = async () => {
+    'use server';
+    const session = await getSession();
+    session.destroy();
+    redirect('/');
+  };
+  return (
+    <div>
+      <div>Welcome! {user?.username}</div>
+      <form action={logOut}>
+        <button>Log Out</button>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
